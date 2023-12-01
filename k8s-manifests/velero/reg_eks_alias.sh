37bb184219ce5c46e87d70a2f73fd96850cfc4fa
ShinChul Bang <shinchul@amazon.com>
2023-12-01 16:17:09 +0900

    edit reg_eks_alias.sh

--- k8s-manifests/velero/reg_eks_alias.sh
+++ k8s-manifests/velero/reg_eks_alias.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+PRIMARY_CLUSTER=eks-blueprint-blue-cluster
 PRIMARY_CONTEXT=primary
-RECOVERY_CONTEXT=recovery
+PRIMARY_CLUSTER_ROLE_ARN=
 
-aws eks update-kubeconfig --name eks-blueprint-blue-cluster --region ap-northeast-2 --alias $PRIMARY_CONTEXT
-aws eks update-kubeconfig --name eks-blueprint-green-cluster --region ap-northeast-2 --alias $RECOVERY_CONTEXT
+RECOVERY_CLUSTER=eks-blueprint-green-cluster
+RECOVERY_CONTEXT=recovery
+RECOVERY_CLUSTER_ROLE_ARN=
+
+REGION=ap-northeast-2
+
+aws eks update-kubeconfig --name $PRIMARY_CLUSTER --region $REGION --role-arn $PRIMARY_CLUSTER_ROLE_ARN --alias $PRIMARY_CONTEXT
+aws eks update-kubeconfig --name $RECOVERY_CLUSTER --region $REGION --role-arn $RECOVERY_CLUSTER_ROLE_ARN --alias $RECOVERY_CONTEXT
